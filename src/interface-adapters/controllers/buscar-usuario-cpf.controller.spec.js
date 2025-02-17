254baed54aecfcce67cc04e0f041778f7788e084
guilherme_git <guilhermeloli.dev@gmail.com>
2025-02-17 17:55:25 -0300

    teste que retorna null se o usuario nao for encontrado por CPF

--- src/interface-adapters/controllers/buscar-usuario-cpf.controller.spec.js
+++ src/interface-adapters/controllers/buscar-usuario-cpf.controller.spec.js
@@ -5,9 +5,6 @@ const buscarUsuarioPorCPFController = require('./buscar-usuario-cpf.controller')
 describe('Buscar usuario por CPF controller', () => {
   const buscarUsuarioPorCPFUseCase = jest.fn();
 
-  // valida os dados da request
-  // envia os dados para o useCase
-  // retorna uma resposta para o usuario
   test('Deve retornar um httpResponse 200 e um usuario se o mesmo for encontrado', async function () {
     const usuarioDTO = {
       id: 'qualquer_id',
@@ -32,6 +29,24 @@ describe('Buscar usuario por CPF controller', () => {
     });
 
     expect(response).toEqual(httpResponse(200, usuarioDTO));
+    expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledTimes(1);
+    expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledWith(httpRequest.params);
+  });
+
+  test('Deve retornar um httpResponse 200 e null caso o usuario nao for encontrado por CPF', async () => {
+    buscarUsuarioPorCPFUseCase.mockResolvedValue(Either.Right(null));
+    const httpRequest = {
+      params: {
+        CPF: '123.123.123-12',
+      },
+    };
+
+    const response = await buscarUsuarioPorCPFController({
+      buscarUsuarioPorCPFUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(200, null));
     expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledTimes(1);
     expect(buscarUsuarioPorCPFUseCase).toHaveBeenCalledWith(httpRequest.params);
   });
