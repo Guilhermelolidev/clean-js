148c1bd67ed227851c2fb5b0db4b3a51ae655665
guilherme_git <guilhermeloli.dev@gmail.com>
2025-02-20 17:09:58 -0300

    emprestimos controller / tests

--- src/application/buscar-emprestimos-pendentes.spec.js
+++ src/application/buscar-emprestimos-pendentes.spec.js
@@ -1,5 +1,6 @@
 // - Emprestimo (usuario_id, livro_id, data_retorno, data_devolucao, data_saida)
 
+const emprestimosPendentesFixture = require('../../tests/fixtures/buscar-emprestimos-pendentes');
 const { AppError } = require('../shared/errors');
 const buscarEmprestimosPendentesUsecase = require('./buscar-emprestimos-pendentes.usecase');
 
@@ -15,30 +16,9 @@ describe('Buscar emprestimos pendentes UseCase', function () {
   });
 
   test('Buscar emprestimos pendentes', async () => {
-    emprestimosRepository.buscarPendentesComLivroComUsuario.mockResolvedValue([
-      {
-        usuario: {
-          nome: 'qualquer_nome_valido',
-          CPF: 'qualquer_cpf',
-        },
-        livro: {
-          nome: 'qualquer_nome_livro',
-        },
-        data_saida: '2024-10-01',
-        data_saida: '2024-10-02',
-      },
-      {
-        usuario: {
-          nome: 'qualquer_nome_usuario',
-          CPF: 'qualquer_cpf_valido',
-        },
-        livro: {
-          nome: 'qualquer_nome_livro_valido',
-        },
-        data_saida: '2024-10-01',
-        data_saida: '2024-10-02',
-      },
-    ]);
+    emprestimosRepository.buscarPendentesComLivroComUsuario.mockResolvedValue(
+      emprestimosPendentesFixture
+    );
 
     const sut = buscarEmprestimosPendentesUsecase({ emprestimosRepository });
     const output = await sut();
--- src/interface-adapters/controllers/buscar-emprestimos-pendentes.controller.spec.js
+++ src/interface-adapters/controllers/buscar-emprestimos-pendentes.controller.spec.js
@@ -1,31 +1,25 @@
-const { Either } = require('../../shared/errors');
+const emprestimosPendentesFixture = require('../../../tests/fixtures/buscar-emprestimos-pendentes');
+const { Either, AppError } = require('../../shared/errors');
 const httpResponse = require('../../shared/helpers/http.response');
 const buscarEmprestimosPendentesController = require('./buscar-emprestimos-pendentes.controller');
 
 describe('Buscar emprestimos pendentes Controller', () => {
   const buscarEmprestimosPendentesUseCase = jest.fn();
-  test('Deve retornar os empréstimos pendentes', async () => {
-    const emprestimosDTO = [
-      {
-        usuario: {
-          nome: 'qualquer_nome_valido',
-          CPF: 'qualquer_cpf',
-        },
-        livro: {
-          nome: 'qualquer_nome_livro',
-        },
-        data_saida: '2024-10-01',
-        data_saida: '2024-10-02',
-      },
-    ];
+  test('Deve retornar um httpResponse 200 e os empréstimos pendentes', async () => {
     buscarEmprestimosPendentesUseCase.mockResolvedValue(
-      Either.Right(emprestimosDTO)
+      Either.Right(emprestimosPendentesFixture)
     );
     const response = await buscarEmprestimosPendentesController({
       buscarEmprestimosPendentesUseCase,
     });
 
-    expect(response).toEqual(httpResponse(200, emprestimosDTO));
+    expect(response).toEqual(httpResponse(200, emprestimosPendentesFixture));
     expect(buscarEmprestimosPendentesUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test('Deve retornar um throw app error se o useCase não for fornecido', () => {
+    expect(() => buscarEmprestimosPendentesController({})).rejects.toThrow(
+      new AppError(AppError.dependencias)
+    );
   });
 });
