3a850cc4b078efa8fd7483c62604bca451697d20
guilherme_git <guilhermeloli.dev@gmail.com>
2025-02-28 19:46:31 -0300

    typeorm example

---
+++ src/infra/db/typeorm/typeorm.prod.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: '',
+  host: '',
+  database: '',
+  username: '',
+  password: '',
+  port: 5432,
+  synchronize: false,
+  entities: [resolve(__dirname, 'entities/*.entity-typeorm.js')],
+};
